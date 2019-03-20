7b0cb5f4fd53428b1072b41b114e0c370b5dd620
admin <3ugol@ukr.net>
2019-03-20 15:16:48 +0200

    Initial commit

---
+++ src/BouncingBall.java
@@ -0,0 +1,16 @@
+public class BouncingBall {
+ /*   public static int BouncingBall(double h, double bounce, double window){
+        h=3;
+        bounce = 0.66;
+        window = 1.5;
+        int result =1;
+
+        if (h > window) {
+            h = h * bounce;
+            if (h > window) result=+2 ;
+        }
+        return result;
+        System.out.println("result");
+    }
+*/
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+       // BouncingBall bBall = new BouncingBall();
+        System.out.println(BouncingBall(3, 0.66, 1.5));
+    }
+
+    public static int BouncingBall(double h, double bounce, double window) {
+        //h = 3;
+        //bounce = 0.66;
+        //window = 1.5;
+        int result = 1;
+            for (; h > window;h = h * bounce) {
+                //h = h * bounce;
+                if (h > window) result++;
+
+
+                //System.out.println("result");
+            }
+
+
+        return result;
+    }
+        static void hello() {
+            System.out.println("Ok");
+        }
+}
