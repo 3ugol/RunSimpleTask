b9c7363a0d8d220d50c87c84eb20d1b6bc29e411
my comp <3ugol@ukr.net>
2019-03-26 11:08:27 +0200

    Max sum

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Keypad.presses("WHERE DO U WANT 2 MEET L8R"));
-        //System.out.println(WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
-        //System.out.println(ArithmeticFunction.arithmetic(2,2,"add"));
-       // BouncingBall
-       // System.out.println(BouncingBall.bouncingBall(30.0, 0.66, 1.5));
+
+//        int [] arr = {-21, -12, -16, -14, 11, -16, 29, -2, -2, 1, -30, 26, -2, 28, -1, 4, -19, -28, -9, 4, -15, 15, -13, 13, -25, 11, 18, 16, -10, 12, -16, 17, 3, -21, 17, -20, 27, 22, -12, 22, -7, 13, 14, 7, 3, 25, 16, 9, -30, 25};
+//        System.out.println(Max.sequence( arr ));
+//        System.out.println(Keypad.presses("WHERE DO U WANT 29 MEET L8R"));
+//        System.out.println(WeightSort.orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+//        System.out.println(ArithmeticFunction.arithmetic(2,2,"add"));
+//        BouncingBall
+//        System.out.println(BouncingBall.bouncingBall(30.0, 0.66, 1.5));
     }
 }
---
+++ src/Max.java
@@ -0,0 +1,19 @@
+public class Max {
+    public static int sequence(int[] arr) {
+        int max=0 ,in=0, out=0;
+        for (int i = 0; i < arr.length; i++) {
+        int i1=0, j=0;
+            for (j+=i; j < arr.length; j++) {
+                i1 += arr[j];
+                if (i1>max) {
+                    max = i1;
+                    in = i;
+                    out = j;
+                }
+            }
+            System.out.println("ot" + in + "do" + out + " max" + max);
+        }
+        return max;
+    }
+}
+
