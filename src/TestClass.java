8b7865cb20248dfb6634de2f304430938b3914cf
my comp <3ugol@ukr.net>
2019-04-17 19:00:22 +0300

    т

---
+++ src/TestClass.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestClass {
+//    public static void main(String[] args) {
+//        Integer integer = 2;
+//        BigInteger bigInteger = BigInteger.valueOf(integer);
+//        boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(integer));
+//        System.out.println(probablePrime);
+//    }
+public static void main(String[] args) {
+//    int s=110;
+//    boolean b=true;
+//    List <Integer> list = new ArrayList<>();
+//    List <Integer> list2 = new ArrayList<>();
+//
+//    for (int i = 100; i < s; i++) {
+//        for (int j = 2; j < i; j++) {
+//            if (i%j == 0) {
+////                System.out.println(j);
+//                b=false;
+//                break;
+//            }
+////            else System.out.println(j);
+//        }
+//        if (b) System.out.println(i);
+//        else b = true;
+//        list.add(i);
+//
+//    }
+//    for (int q=1; list.size()>2 && q < list.size(); q++)  {
+//        int z=0, x=0, y=1;
+//        if (z == list.get(y) - list.get(x)) {
+//            list2.add(list.get(x));
+//            list2.add(list.get(y));
+//        }
+//        x++;
+//        y++;
+//    }
+//
+}
+}
